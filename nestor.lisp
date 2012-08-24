c63cf3f2d8eec290d4265d176963f07e755d6abf
João Távora <joaotavora@gmail.com>
2012-08-24 01:38:53 +0100

    Add: GPL3

--- nestor.lisp
+++ nestor.lisp
@@ -1,3 +1,23 @@
+;;; nestor.lisp --- File-based blogging, cloning the excellent github.com/gma/nesta
+
+;;; Copyright (C) 2012 Joao Tavora
+
+;;; Author: Joao Tavora <joaotavora@gmail.com>
+;;; Keywords:
+
+;;; This program is free software; you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation, either version 3 of the License, or
+;;; (at your option) any later version.
+
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (require 'hunchentoot)
   (require 'cl-who)
@@ -105,7 +125,6 @@
   (:documentation "Return a string rendering PAGE of TYPE")
   (:method (page (type (eql :mdown)))
     (cl-markdown:render-to-stream (cl-markdown:markdown (page-content page)) :html nil)))
-
 
 (defun page-dispatcher (request)
   (let (file)
